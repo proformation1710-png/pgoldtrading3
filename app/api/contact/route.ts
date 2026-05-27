2e5664e27ad20d2b8be431e3c6d9b4e564de427e
User <user@MacBook-Pro-de-User.local>
2026-05-27 21:13:46 +0200

    test api

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,32 +1,7 @@
-import { Resend } from "resend"
-
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 export async function POST(req: Request) {
-  try {
-    const body = await req.json()
+  console.log("API CONTACT WORKS")
 
-    const { name, email, message } = body
-    
-    console.log("SENDING EMAIL")
-
-    await resend.emails.send({
-      from: "PGOLD <onboarding@resend.dev>",
-      to: "proformation1710@gmail.com",
-      subject: "New message PGOLDTRADING",
-      html: `
-        <h1>New Contact</h1>
-        <p>Name: ${name}</p>
-        <p>Email: ${email}</p>
-        <p>Message: ${message}</p>
-      `,
-    })
-
-    return Response.json({ success: true })
-  } catch (error) {
-    return Response.json(
-      { error: "Email failed" },
-      { status: 500 }
-    )
-  }
+  return Response.json({
+    success: true,
+  })
 }
